d6484e9d7975df04eb7683acdcaa84c62cbe5b13
EmsCodes <69721898+EmsCodes@users.noreply.github.com>
2021-03-19 20:48:43 +0100

    IxD
    Updated/fixed/added to many elements on index, movies and a bit more.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const confirmBox = document.querySelector("#confirm-box");
+const movieContainer = document.querySelector(".container");
+
+movieContainer.onclick = function(){
+    confirmBox.style.display = "block";
+    // document.main.style.filter = "blur(1px)";
+}
+
+
+
+
+
